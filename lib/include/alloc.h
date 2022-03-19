4d38600a2c7b45aeb970d7644a04b4793979cfe0
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2022-03-19 15:04:20 -0300

    Macro to use malloc and free from C stdlib.

--- lib/include/alloc.h
+++ lib/include/alloc.h
@@ -1,8 +1,16 @@
 #ifndef IGOLT_ALLOC_H
 #define IGOLT_ALLOC_H
 
+#ifdef USE_CSTD
+#include <stdlib.h>
+
+#define alloc(size)  malloc(size)
+#define memfree(ptr) free(ptr)
+#else
 #include <stddef.h>
 
 void *alloc(size_t n);
+#define memfree(ptr) do /* Not implemented yet */ while (0);
+#endif /* USE_CSTD */
 
 #endif /* IGOLT_ALLOC_H */
